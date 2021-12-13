6931df120a3a9a3aa5f5a05831579f52092b303e
Matt Chun-Lum <matt@questionable.engineering>
2021-12-12 22:40:18 -0800

    fix: multiline in markdown table rows is hard

--- scripts/format_example_output.py
+++ scripts/format_example_output.py
@@ -1,7 +1,11 @@
 import sys
 import json
 
+def sanitize(solution):
+    s = f"{solution}".replace('\n', '<br>')
+    return f"<pre>{s}</pre>"
+
 solution = json.load(sys.stdin)
-p1 = f"{solution['part_one']}".replace('\n', '<br>')
-p2 = f"{solution['part_two']}".replace('\n', '<br>')
+p1 = sanitize(solution['part_one'])
+p2 = sanitize(solution['part_two'])
 print(f"{p1}|{p2}")
