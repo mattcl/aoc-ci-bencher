bcd9720d12faf8338e96832be53f5c5cfb4bcbf8
Matt Chun-Lum <matt@questionable.engineering>
2021-12-12 22:31:56 -0800

    fix: make sure things are actually strings

--- scripts/format_example_output.py
+++ scripts/format_example_output.py
@@ -2,6 +2,6 @@ import sys
 import json
 
 solution = json.load(sys.stdin)
-p1 = solution['part_one'].replace('\n', '<br>')
-p2 = solution['part_two'].replace('\n', '<br>')
+p1 = f"{solution['part_one']}".replace('\n', '<br>')
+p2 = f"{solution['part_two']}".replace('\n', '<br>')
 print(f"{p1}|{p2}")
