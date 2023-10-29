80d1ed4b21782d5fe3635085c31a2d242648b7a9
Matt Chun-Lum <matt@questionable.engineering>
2023-10-28 19:34:40 -0700

    fix: typos in comparative-benchmarks script

--- 2023/scripts/comparative-benchmarks.sh
+++ 2023/scripts/comparative-benchmarks.sh
@@ -8,7 +8,7 @@ mv aoc-tools /usr/local/bin/
 cd ../
 
 # unpack and install the mattcl solution
-cd repo
+cd mattcl-bin
 tar -xvf aoc-*-x86_64-unknown-linux-gnu.tar.gz
 mv aoc /usr/local/bin/mattcl-aoc
 cd ../
@@ -23,7 +23,7 @@ INPUTS_DIR="aoc-inputs-write/${YEAR}"
 i=1
 while [ $i -ne 26 ];
 do
-    aoc-tools bench "$1" "$INPUTS_DIR"
-    aoc-tools report "$1" "$INPUTS_DIR" -t ci/templates/report-template.md -o README.md
+    aoc-tools bench "$i" "$INPUTS_DIR"
+    aoc-tools report "$i" "$INPUTS_DIR" -t ci/templates/report-template.md -o README.md
     i=$((i + 1))
 done
