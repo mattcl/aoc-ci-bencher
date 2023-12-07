6f5af7d609ddff8ee0adfe30d55b9f98693d7195
Matt Chun-Lum <matt@questionable.engineering>
2023-12-06 20:29:46 -0800

    chore: start benches at day 4

--- 2023/scripts/comparative-benchmarks.sh
+++ 2023/scripts/comparative-benchmarks.sh
@@ -64,7 +64,7 @@ INPUTS_DIR="aoc-inputs-write/${YEAR}"
 set +x
 # we don't need the script debugging from this point on
 
-i=3
+i=4
 while [ $i -ne 26 ];
 do
     aoc-tools ci bench "$YEAR" "$i" "$INPUTS_DIR"
