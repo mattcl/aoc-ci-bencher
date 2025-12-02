707ec2c6a4eefb61e20dcd44537cfc723c818d7f
Matt Chun-Lum <matt@questionable.engineering>
2025-12-01 19:55:46 -0800

    chore: kcen bench

--- 2025/scripts/comparative-benchmarks.sh
+++ 2025/scripts/comparative-benchmarks.sh
@@ -32,11 +32,11 @@ tar -xvf whyando-aoc-*-x86_64-unknown-linux-gnu.tar.gz
 mv aoc-2025 /usr/local/bin/whyando-aoc
 cd ../
 
-# unpack and install the kcen solution
-# cd kcen-bin
-# tar -xvf kcen-aoc-*.tar.gz
-# mv kcen-aoc /usr/local/bin/kcen-aoc
-# cd ../
+unpack and install the kcen solution
+cd kcen-bin
+tar -xvf kcen-aoc-*.tar.gz
+mv kcen-aoc /usr/local/bin/kcen-aoc
+cd ../
 
 # install mattcl-py
 # cd mikofo
