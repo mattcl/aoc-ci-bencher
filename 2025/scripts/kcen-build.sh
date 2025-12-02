3043228c71d286f44469110e308b9cb69caf5039
Matt Chun-Lum <matt@questionable.engineering>
2025-12-01 20:46:50 -0800

    feat: attempt-new-nim

--- 2025/scripts/kcen-build.sh
+++ 2025/scripts/kcen-build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -e
 
-nimble --out:dist/kcen-aoc -d:danger --mm:orc -d:lto --passC:-march=native c aoc.nim
+./ci/scripts/build.sh
+# nimble --out:dist/kcen-aoc -d:danger --mm:orc -d:lto --passC:-march=native c aoc.nim
 
 # cp the target release for a later step
 mkdir tmp
